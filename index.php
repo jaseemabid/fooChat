73c3c15feb8419c780294865c06adc98950e1017
Jaseem Abid <jaseemabid@gmail.com>
2012-04-03 17:13:40 +0530

    Trivial base version

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<meta name="description" content="fooChat" />
+		<meta name="author" content="Jaseem Abid" />
+		<title>fooChat</title>
+
+		<!-- Less styles -->
+		<link rel="stylesheet" href="static/css/bootstrap.min.css" />
+		<link rel="stylesheet/less" type="text/css" href="static/css/style.less">
+	</head>
+	<body>
+		<div class="container">
+		</div>
+
+		<script src="static/js/foochat.min.js"></script>
+	</body>
+</html>
