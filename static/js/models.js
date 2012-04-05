854ed22dd0b0435d8777b1d077e215583a44eadc
Jaseem Abid <jaseemabid@gmail.com>
2012-04-05 23:38:18 +0530

    Copy paste bug

--- static/js/models.js
+++ static/js/models.js
@@ -8,7 +8,6 @@ fooChat.Models = {
 	Contact : Backbone.Model.extend({
 		// Default attributes for the contact
 		defaults: {
-			// Ensure that each photo created has an `src`.
 			fullName : "fooBar",
 			hash: "fooBar"
 		},
