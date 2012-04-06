d6fe17bf95ca6add0463f87c3f9210bcd7dcdaec
Vipin Nair <swvist@gmail.com>
2012-04-07 02:39:44 +0530

    Added URL rule for root

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask
+import views
+
+app = Flask(__name__)
+
+"""
+URL Settings
+"""
+app.add_url_rule('/', 'index', view_func = views.index)
+
+if __name__ == '__main__':
+	app.run(port=50000)
+
