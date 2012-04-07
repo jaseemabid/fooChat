b960a18276a883b05eadfab66415f045365fd61f
Jaseem Abid <jaseemabid@gmail.com>
2012-04-07 20:43:48 +0530

    Sample login code for UI test

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ app = Flask(__name__)
 URL Settings
 """
 app.add_url_rule('/', 'index', view_func = views.index)
+app.add_url_rule('/login', 'login', view_func = views.login, methods=['GET', 'POST'])
+
 
 if __name__ == '__main__':
 	app.debug = True;
--- views.py
+++ views.py
@@ -4,3 +4,13 @@ log.basicConfig(level=log.DEBUG)
 def index():
 	log.info("Index function called.")
 	return "Hello world."
+
+def login():
+	log.info("Login function called.")
+	# Demo login, write function later.
+	return '{ \
+		"uid":"3d123352c8c4c8866f5158acc6000366", \
+		"fullName" : "Jaseem Abid" \
+		}'
+
+
