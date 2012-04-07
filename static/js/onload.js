f481c12f75e32d84aaf04d2fa4ca08c0c24025f9
Jaseem Abid <jaseemabid@gmail.com>
2012-04-08 02:40:22 +0530

    Demo login

--- static/js/onload.js
+++ static/js/onload.js
@@ -1,13 +1,23 @@
 /*!
-  * The main fooChat code to be excecuted on page load.
-*/
+ * The main fooChat code to be excecuted on page load.
+ */
 
 (function ($) {
 	"use strict";
 	$(document).ready(function () {
-		window.User = new fooChat.Models.ActiveUser();
+
+		var jaseem = {
+			uid: 1234,
+			fullname: 'Jaseem Abid',
+			username: 'jaseemabid',
+			hash: "40901f06ff8e7bb58e200630c613d647"
+		};
+
+		window.User = new fooChat.Models.ActiveUser(jaseem);
 		window.App = new fooChat.Routers.BackboneApp();
-		Backbone.history.start({root: "/fooChat"});
+		Backbone.history.start({
+			root: "/fooChat"
+		});
 
 	});
 
--- views.py
+++ views.py
@@ -9,8 +9,8 @@ def login():
 	log.info("Login function called.")
 	# Demo login, write function later.
 	return '{ \
-		"uid":"3d123352c8c4c8866f5158acc6000366", \
-		"fullName" : "Jaseem Abid" \
-		}'
-
-
+			"uid": 1234, \
+			"fullname": "Jaseem Abid", \
+			"username": "jaseemabid", \
+			"hash": "40901f06ff8e7bb58e200630c613d647" \
+			}'
