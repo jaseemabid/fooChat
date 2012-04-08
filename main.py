1c1d17f577f5f84898acb3a2d31b4e4a21efaada
Vipin Nair <swvist@gmail.com>
2012-04-08 06:40:51 +0530

    Added path for login, logout and register

--- main.py
+++ main.py
@@ -7,8 +7,9 @@ app = Flask(__name__)
 URL Settings
 """
 app.add_url_rule('/', 'index', view_func = views.index)
-app.add_url_rule('/login', 'login', view_func = views.login, methods=['GET', 'POST'])
-
+app.add_url_rule('/user/login/', 'login', view_func = views.login, methods=['GET', 'POST'])
+app.add_url_rule('/user/logout/', 'logout', view_func = views.logout)
+app.add_url_rule('/user/register/', 'register', view_func = views.register, methods=['GET'])
 
 if __name__ == '__main__':
 	app.debug = True;
