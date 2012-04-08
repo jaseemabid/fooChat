e67844903c599387d51dde62635fa86a90f7bef0
Vipin Nair <swvist@gmail.com>
2012-04-08 06:41:53 +0530

    Added methods for logout and register. Connected to couch.

--- views.py
+++ views.py
@@ -1,5 +1,12 @@
+from flask import jsonify, request
+from commons import get, post
+import settings
+import couchdb
 import logging as log
 log.basicConfig(level=log.DEBUG)
+
+couch = couchdb.Server()
+db = couch[settings.database]
 
 def index():
 	log.info("Index function called.")
@@ -14,3 +21,18 @@ def login():
 			"username": "jaseemabid", \
 			"hash": "40901f06ff8e7bb58e200630c613d647" \
 			}'
+def logout():
+	log.info("Logout function called.")
+	return jsonify(success="true")
+
+def register():
+	if get():
+		data=dict()
+		data['username']=request.args.get('username')
+		data['password']=request.args.get('password')
+		data['email']=request.args.get('email')
+		db.save(data)
+		if data.has_key('_id'):
+			return jsonify(success='true',data=data)
+		else:
+			return jsonify(success="false")
