fc90ac000b0696b868c9d76538df27e1b702f7c6
Jaseem Abid <jaseemabid@gmail.com>
2012-04-09 18:04:34 +0530

    Redirect if not logged in

--- static/js/routers.js
+++ static/js/routers.js
@@ -13,10 +13,12 @@ fooChat.Routers = {
 		initialize: function () {},
 		home: function () {
 			fooChat.activeUser = new fooChat.Models.ActiveUser();
-			//				fooChat.activeUser.set({'uid':'3d123352c8c4c8866f5158acc6000366'}); // jaseemabid's
-			fooChat.activeUser.set({
-				'uid': '3d123352c8c4c8866f5158acc60010d7'
-			}); // swvist's
+			if(fooChat.activeUser.get('uid') === 0 ) {
+				fooChat.appRouter.navigate('/login', true);
+				return;
+			}
+   			//fooChat.activeUser.set({'uid':'3d123352c8c4c8866f5158acc6000366'}); // jaseemabid's
+			//fooChat.activeUser.set({'uid': '3d123352c8c4c8866f5158acc60010d7'}); // swvist's
 			fooChat.contacts = new fooChat.Collections.ContactList([], fooChat.activeUser);
 			fooChat.messages = new fooChat.Collections.MessageList([], fooChat.activeUser);
 			fooChat.topBar = new fooChat.Views.TopBarView(fooChat.activeUser);
@@ -24,6 +26,7 @@ fooChat.Routers = {
 		},
 		login: function () {
 			console.log("login");
+			fooChat.activeUser = new fooChat.Models.ActiveUser();
 		},
 		blank: function () {
 			console.log("blank");
