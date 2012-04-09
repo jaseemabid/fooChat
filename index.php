0b539b526ed987db6f354c9d22e3782b480a6728
Jaseem Abid <jaseemabid@gmail.com>
2012-04-09 19:11:27 +0530

    Login template

--- index.php
+++ index.php
@@ -35,6 +35,14 @@
 	<div class=navbar><div class=navbar-inner><div class=container><a class=brand href="#">Welcome {fullname}</a><ul class="nav pull-right" id=dashBoard><li class="dropdown active"><a href="#" class=dropdown-toggle data-toggle=dropdown><i class="icon-user icon-white"></i>&nbsp;&nbsp;&nbsp;{fullname}<b class=caret></b></a><ul class=dropdown-menu><img src="http://www.gravatar.com/avatar/{hash}"><ul class=userInfo><li><strong>{fullname}</strong></li><li>{email}</li><li>{username}</li></ul><li class=divider></li><li><a id=signout href="#logout">Signout</a><a id=settings href="#settings">Settings</a></li></ul></li></ul></div></div></div>
 </script>
 
+<script id="template-login" type="text/template">
+<form class="well form-vertical"><div class="input-prepend">
+<label>Username</label><span class="add-on"><i class="icon-user"></i></span><input class="span10" placeholder="Username">
+</div><div class="input-prepend">
+<label>Password</label><span class="add-on"><i class="icon-lock"></i></span><input class="span10" placeholder="Username">
+</div><button type="submit" class="btn">Sign in</button></form>
+</script>
+
 		<div class="container-fluid">
 
 			<div class="row-fluid">
