9789b6dbe52aa657f14494bf8c2db6ede0c16580
gayathri pp <pp.gayathri4u2@gmail.com>
2012-04-19 17:11:52 +0530

    functions for addcontact

--- commons.py
+++ commons.py
@@ -1,4 +1,5 @@
 from flask import jsonify, request
+from models import db
 import hashlib
 import settings
 
@@ -16,3 +17,28 @@ def post():
 
 def encryptPassword(password):
 	return hashlib.sha1(password+settings.salt).hexdigest()
+	
+def checkUsernameExists(username):
+	user = db.view('byUsername/userId',key=username)
+	if  len(user.rows)  > 0:
+		return True
+	else:
+		return False
+		
+def emailHash(email):
+	return hashlib.sha1(email).hexdigest()
+			
+def checkContactExists(userId,contactName,contactId):
+	user = db.view('byUserId/contacts',key=userId).rows[0]
+	return user["value"].__contains__({"username":contactName,"hash":contactId})
+	
+def addContact(userId,contactName,contactId):
+	if checkContactExists(userId,contactName,contactId) is False:
+		user=db[userId]
+		user["contacts"].append({"username":contactName,"hash":contactId})
+		db.save(user)
+		return True
+	
+		
+			
+			
